e1b596e4018500d611290ebea6f4a68523b57d9d
Daivy Morales <daivymorales7705@gmail.com>
2023-04-26 16:04:51 -0500

    Add updating field in Product Table

--- src/components/product/CardProduct.tsx
+++ src/components/product/CardProduct.tsx
@@ -1,3 +1,8 @@
+import { FaProductHunt } from "react-icons/fa";
+import { useContext, useState } from "react";
+import { useFormik } from "formik";
+import { productContext } from "@/context/ProductContextProvider";
+
 interface IProduct {
   Codigo: number;
   Descripcion: string;
@@ -12,16 +17,85 @@ interface MyProps {
 }
 
 export default function CardProduct({ product }: MyProps) {
+  const { fieldOn, setFieldOn, updateProduct } = useContext(productContext);
+
+  const [productSchema, setProductSchema] = useState({
+    Codigo: product.Codigo,
+    Descripcion: product.Descripcion,
+    Presentacion: product.Presentacion,
+  });
+
+  const formik = useFormik({
+    initialValues: { productSchema },
+    onSubmit: async (values) => {
+      console.log(values.productSchema);
+      setProductSchema(values.productSchema);
+      await updateProduct(product.Codigo, values.productSchema);
+      setFieldOn("n");
+    },
+    enableReinitialize: true,
+  });
+
   return (
-    <tr className="text-xs border-b border-gray-700">
+    <tr
+      className={`text-xs border-b border-gray-700 text-gray-300 font-normal ${
+        fieldOn === product._id ? "bg-gray-900" : ""
+      }`}
+      onClick={() => setFieldOn(product._id)}
+    >
       <th
         scope="row"
-        className="px-3 py-4 whitespace-nowrap"
+        className="px-3 py-1 "
+        onClick={() => setFieldOn(product._id)}
       >
-        {product.Codigo}
+        {fieldOn === product._id ? (
+          <form onSubmit={formik.handleSubmit}>
+            <input
+              className="fieldTable w-20"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.productSchema.Codigo}
+              name="productSchema.Codigo"
+            />
+          </form>
+        ) : (
+          product.Codigo
+        )}
       </th>
-      <td className="px-3 py-4">{product.Descripcion}</td>
-      <td className="px-3 py-4">{product.Presentacion}</td>
+      <td
+        className="px-3 py-1 flex justify-start items-center gap-x-1 text-white "
+        onClick={() => setFieldOn(product._id)}
+      >
+        {fieldOn === product._id ? (
+          <form onSubmit={formik.handleSubmit}>
+            <input
+              className="fieldTable w-60"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.productSchema.Descripcion}
+              name="productSchema.Descripcion"
+            />
+          </form>
+        ) : (
+          product.Descripcion
+        )}
+        <FaProductHunt size={10} className="text-blue-500" />
+      </td>
+      <td className="px-3 py-1" onClick={() => setFieldOn(product._id)}>
+        {fieldOn === product._id ? (
+          <form onSubmit={formik.handleSubmit}>
+            <input
+              className="fieldTable w-12"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.productSchema.Presentacion}
+              name="productSchema.Presentacion"
+            />
+          </form>
+        ) : (
+          product.Presentacion
+        )}
+      </td>
     </tr>
   );
 }
--- src/context/ProductContextProvider.tsx
+++ src/context/ProductContextProvider.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { ReactNode, createContext, useState } from "react";
 
 interface ProductContextProps {
@@ -16,19 +17,44 @@ interface IProduct {
 interface IContext {
   products: IProduct[];
   setProducts: React.Dispatch<React.SetStateAction<IProduct[]>>;
+  fieldOn: string;
+  setFieldOn: React.Dispatch<React.SetStateAction<string>>;
+  updateProduct: (Codigo: number, body: object) => Promise<void>;
 }
 
 export const productContext = createContext<IContext>({
   products: [],
-  setProducts: async () => {},
+  setProducts: () => {},
+  fieldOn: "n",
+  setFieldOn: () => {},
+  updateProduct: async () => {},
 });
 
 export const ProductContextProvider = ({ children }: ProductContextProps) => {
   const [products, setProducts] = useState<IProduct[]>([]);
-  console.log(products);
+  const [fieldOn, setFieldOn] = useState<string>("n");
+
+  const updateProduct = async (Codigo: number, body: object) => {
+    try {
+      const response = await axios.put(`/api/product/${Codigo}`, body);
+      setProducts(
+        products.map((product) => {
+          if (product.Codigo === Codigo) {
+            return response.data;
+          } else {
+            return product;
+          }
+        })
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <productContext.Provider value={{ products, setProducts }}>
+    <productContext.Provider
+      value={{ products, setProducts, fieldOn, setFieldOn, updateProduct }}
+    >
       {children}
     </productContext.Provider>
   );
