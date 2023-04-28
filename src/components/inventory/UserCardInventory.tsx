efc2395915040b76ee9305d9b9ee6feaa5014f21
Daivy Morales <daivymorales7705@gmail.com>
2023-04-27 19:03:06 -0500

    Add profiles by URL

---
+++ src/components/inventory/UserCardInventory.tsx
@@ -0,0 +1,177 @@
+import { FaProductHunt } from "react-icons/fa";
+import { useContext, useState, useEffect } from "react";
+import { useFormik } from "formik";
+import { productContext } from "@/context/ProductContextProvider";
+import { inventoryContext } from "@/context/InventoryContextProveider";
+
+interface IInventory {
+  Codigo: number;
+  Descripcion: string;
+  Lote: string;
+  Almacen: number;
+  Cantidad: number;
+  Conteo: number;
+  Saldo: number;
+  Formula: number;
+  _id: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface MyProps {
+  inventory: IInventory;
+}
+
+export default function UserCardInventory({ inventory }: MyProps) {
+  const { fieldOn, setFieldOn, products, columnOn } =
+    useContext(productContext);
+
+  const { updateInventory } = useContext(inventoryContext);
+
+  const [presentacionLoad, setPresentacionLoad] = useState({
+    Presentacion: 0,
+  });
+
+  useEffect(() => {
+    products.filter((product) => {
+      product.Codigo === inventory.Codigo
+        ? setPresentacionLoad({
+            Presentacion: product.Presentacion,
+          })
+        : "";
+    });
+  }, []);
+
+  const [inventorySchema, setInventorySchema] = useState({
+    Conteo: inventory.Conteo === undefined ? 0 : inventory.Conteo,
+    Saldo: inventory.Saldo === undefined ? 0 : inventory.Saldo,
+    Formula: inventory.Formula === undefined ? 0 : inventory.Formula,
+  });
+
+  const formik = useFormik({
+    initialValues: { inventorySchema },
+    onSubmit: async (values, { resetForm }) => {
+      console.log(values.inventorySchema);
+      await updateInventory(inventory._id, values.inventorySchema);
+      setInventorySchema(values.inventorySchema);
+      setFieldOn("n");
+      resetForm();
+    },
+    enableReinitialize: true,
+  });
+
+  let total: number = Math.floor(
+    inventory.Conteo * presentacionLoad.Presentacion +
+      inventory.Saldo +
+      inventory.Formula
+  );
+
+  if (isNaN(total)) {
+    total = 0;
+  }
+
+  return (
+    <>
+      <tr
+        className={`text-xs border-b border-gray-700 text-gray-300 font-normal ${
+          fieldOn === inventory._id ? "bg-gray-900" : ""
+        } hover:bg-gray-900 ${columnOn ? "text-gray-600" : ""}`}
+        onClick={() => setFieldOn(inventory._id)}
+      >
+        <th
+          scope="row"
+          className="px-3 py-1  "
+          onClick={() => setFieldOn(inventory._id)}
+        >
+          {inventory.Codigo}
+        </th>
+        <td
+          className={`px-3 py-1  flex justify-start items-center gap-x-1 ${
+            columnOn ? "text-gray-600" : "text-white"
+          }`}
+          onClick={() => setFieldOn(inventory._id)}
+        >
+          {inventory.Descripcion}
+          <FaProductHunt
+            size={10}
+            className={`${columnOn ? "text-gray-600" : "text-blue-500"}`}
+          />
+        </td>
+        <td className="px-3 py-1" onClick={() => setFieldOn(inventory._id)}>
+          {presentacionLoad.Presentacion}
+        </td>
+        <td className="px-1  py-1" onClick={() => setFieldOn(inventory._id)}>
+          {inventory.Lote}
+        </td>
+        <td className="px-3 py-1" onClick={() => setFieldOn(inventory._id)}>
+          {inventory.Almacen}
+        </td>
+        {/* <td className="px-3 py-1" onClick={() => setFieldOn(inventory._id)}>
+          {inventory.Cantidad}
+        </td> */}
+        <td className="px-3 py-1 " onClick={() => setFieldOn(inventory._id)}>
+          {fieldOn === inventory._id ? (
+            <form onSubmit={formik.handleSubmit}>
+              <input
+                className="fieldTable w-8"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.inventorySchema.Conteo || ""}
+                name="inventorySchema.Conteo"
+              />
+            </form>
+          ) : (
+            inventory.Conteo
+          )}
+        </td>
+
+        <td className="px-3 py-1 " onClick={() => setFieldOn(inventory._id)}>
+          {fieldOn === inventory._id ? (
+            <form onSubmit={formik.handleSubmit}>
+              <input
+                className="fieldTable w-8"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.inventorySchema.Saldo || ""}
+                name="inventorySchema.Saldo"
+              />
+            </form>
+          ) : (
+            inventory.Saldo
+          )}
+        </td>
+
+        {/* <td className="px-3 py-1 " onClick={() => setFieldOn(inventory._id)}>
+          {fieldOn === inventory._id ? (
+            <form onSubmit={formik.handleSubmit}>
+              <input
+                className="fieldTable w-8"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.inventorySchema.Formula || ""}
+                name="inventorySchema.Formula"
+              />
+            </form>
+          ) : (
+            inventory.Formula
+          )}
+        </td> */}
+        <td className="px-3 py-1 ">{total == 0 ? "" : total}</td>
+        {/* <td
+          className={`px-3 py-1 ${
+            total - inventory.Cantidad > 0
+              ? total - inventory.Cantidad === 0
+                ? "text-yellow-600"
+                : "text-gray-400 "
+              : "text-red-500 font-bold"
+          }`}
+        >
+          {total ===
+          presentacionLoad.Presentacion - presentacionLoad.Presentacion
+            ? ""
+            : Math.floor(total - inventory.Cantidad)}
+        </td> */}
+      </tr>
+    </>
+  );
+}
---
+++ src/pages/user.tsx
@@ -0,0 +1,226 @@
+import React, { useState, useEffect, ChangeEvent } from "react";
+import * as xslx from "xlsx";
+import { GetServerSidePropsContext } from "next";
+import axios from "axios";
+import { RiFileExcel2Fill } from "react-icons/ri";
+import UserCardInventory from "../components/inventory/UserCardInventory";
+import { useContext } from "react";
+import { IoSearch } from "react-icons/io5";
+import { productContext } from "@/context/ProductContextProvider";
+import { inventoryContext } from "@/context/InventoryContextProveider";
+import { useFormik } from "formik";
+import ExportButton from "@/components/xlsx/ExportButton";
+import { useRouter } from "next/router";
+
+interface IData {
+  CANTIDAD: number;
+  LOTE: string;
+  NOMBRE: string;
+  PRODUCTO: string;
+}
+
+interface IInventory {
+  Codigo: number;
+  Descripcion: string;
+  Lote: string;
+  Almacen: number;
+  Cantidad: number;
+  Conteo: number;
+  Saldo: number;
+  Formula: number;
+  _id: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface IProduct {
+  Codigo: number;
+  Descripcion: string;
+  Presentacion: number;
+  _id: string;
+  createdAt: string;
+  updateAt: string;
+}
+
+interface MyProps {
+  data1: IInventory[];
+  data2: IProduct[];
+}
+
+export default function index({ data1, data2 }: MyProps) {
+  const { inventoryContent, setInventoryContent, deleteAllInventory } =
+    useContext(inventoryContext);
+
+  const router = useRouter();
+
+  const { setProducts } = useContext(productContext);
+
+  useEffect(() => {
+    setInventoryContent(data1);
+    setProducts(data2);
+  }, []);
+  console.log(inventoryContent);
+
+  const [searchTerm, setSearchTerm] = useState("");
+  console.log(searchTerm);
+  const handleDescripcion = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleFileUpload = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = event.target.files?.[0];
+
+    if (!file) return;
+
+    const fileReader = new FileReader();
+    fileReader.readAsArrayBuffer(file);
+
+    fileReader.onload = async (event) => {
+      if (!event.target) return;
+      const fileBuffer = event.target.result as ArrayBuffer;
+      const workbook = xslx.read(fileBuffer, { type: "array" });
+      const worksheet = workbook.Sheets["Hoja1"];
+      const dataExcel: Array<IData> = xslx.utils.sheet_to_json(worksheet);
+      try {
+        const response = await axios.post("/api/inventory", dataExcel);
+        setInventoryContent(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fileReader.onerror = (event) => {
+      console.error("Error reading file:", event);
+    };
+  };
+
+  return (
+    <div className="flex justify-center items-center">
+      <div className="relative overflow-x-auto  sm:rounded-lg">
+        <div className="flex justify-between items-center gap-x-2 p-5">
+          <div className="flex justify-between items-center gap-x-2 ">
+            {/* {inventoryContent.length > 0 ? (
+              <>
+                <button
+                  className="bg-red-500"
+                  onClick={() => {
+                    deleteAllInventory();
+                    setInventoryContent([]);
+                  }}
+                >
+                  Eliminar
+                </button>
+
+                <ExportButton tableId="my-table" />
+              </>
+            ) : (
+              <label className="buttonExcel flex cursor-pointer gap-x-1 justify-center items-center hover:bg-blue-500">
+                Importar Excel
+                <input
+                  className="hidden"
+                  type="file"
+                  accept=".xlsx"
+                  onChange={handleFileUpload}
+                />
+                <RiFileExcel2Fill />
+              </label>
+            )} */}
+            <div className={`${inventoryContent.length > 0 ? "" : "hidden"}`}>
+              <div className="flex gap-x-1 items-center justify-center  rounded-lg pl-2 py-1">
+                <IoSearch />
+                <input
+                  type="text"
+                  className="text-xs bg-[#1a1c1e] font-semibold  px-1 shadow-sm shadow-gray-900py-1 "
+                  onChange={handleDescripcion}
+                  placeholder="Ej: SULFATO FERROSO"
+                />
+              </div>
+            </div>
+          </div>
+          <a
+            onClick={() => router.push("/product")}
+            className="cursor-pointer hover:text-blue-400"
+          >
+            Ir a productos
+          </a>
+        </div>
+        <table
+          id="my-table"
+          className={`${
+            inventoryContent.length > 0 ? "" : "hidden"
+          }  text-sm text-left text-blue-100 dark:text-blue-100`}
+        >
+          <thead className="text-2xs text-gray-400 uppercase border-b border-gray-700">
+            <tr>
+              <th scope="col" className="px-2">
+                Codigo
+              </th>
+              <th scope="col" className="px-2">
+                Descripcion
+              </th>
+              <th scope="col" className="px-2">
+                Presentacion
+              </th>
+              <th scope="col" className="">
+                Lote
+              </th>
+              <th scope="col" className="px-2">
+                Almacen
+              </th>
+              {/* <th scope="col" className="px-2">
+                Cantidad
+              </th> */}
+              <th scope="col" className="px-2">
+                Conteo
+              </th>
+              <th scope="col" className="px-2">
+                Saldo
+              </th>
+              {/* <th scope="col" className="px-2">
+                Formula
+              </th> */}
+              <th scope="col" className="px-2">
+                Total
+              </th>
+              {/* <th scope="col" className="px-2">
+                Diferencia
+              </th> */}
+            </tr>
+          </thead>
+          <tbody>
+            {inventoryContent
+              .filter((inventory) => {
+                if (searchTerm == "") {
+                  return inventory;
+                } else if (
+                  inventory.Descripcion &&
+                  inventory.Descripcion.includes(searchTerm)
+                ) {
+                  return inventory;
+                }
+              })
+              .map((inventory) => (
+                <UserCardInventory inventory={inventory} key={inventory._id} />
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const res1 = await fetch(
+    "https://albatec-inventory.vercel.app/api/inventory"
+  );
+  const data1 = await res1.json();
+
+  const res2 = await fetch("https://albatec-inventory.vercel.app/api/product");
+  const data2 = await res2.json();
+
+  return {
+    props: { data1, data2 },
+  };
+}
